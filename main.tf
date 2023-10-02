38ac04cedae5bb99e7b4f18baf9fee02bb121d28
sw-micaramirez <micaela.ramirez@southworks.com>
2023-10-02 12:45:25 -0300

    test

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "function_delete_old_buildbots" {
-  source                             = "./function"
+  source                             = "./modules-terraform/function"
   path                               = "${path.module}/../delete-old-buildbots-function"
   name                               = "DeleteOldBuildbots"
   function_name                      = "ucb-mgue-delete-old-buildbots-func001"
@@ -17,7 +17,7 @@ module "function_delete_old_buildbots" {
 }
 
 module "function_check_buildbots_on_builders" {
-  source                             = "./function"
+  source                             = "./modules-terraform/"
   path                               = "${path.module}/../buildbots-check-on-builders-function"
   name                               = "CheckBuildbotsOnBuilders"
   function_name                      = "ucb-mgue-check-on-builders-func001"
